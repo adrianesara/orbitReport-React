aee125dab378ef91b1cd0f512498f9f33b11ee6c
adrianesara <adrianesara@hotmail.com>
2023-12-06 16:56:25 -0600

    Project complete, passes all tests

---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+function Banner() {
+    return (
+    <div>
+        <hi>Orbit Report</hi>
+        <p>Click the buttons to see the satelites in that orbit type</p>
+    </div>
+    );
+}
+
+export default Banner
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,34 @@
+import satData from './satData';
+
+
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+     {sat.map((data, id) => {
+     const opStat = data.operational;
+     return (
+    
+    <tr key={id}>
+    
+    <td>{data.name}</td>
+    <td>{data.type}</td>
+    <td>{data.launchDate}</td>
+   <td>{opStat ? "Active" : "Inactive"}</td>
+    
+   
+    </tr>
+     )})}  
+
+         
      </tbody>
    </table>
   );
